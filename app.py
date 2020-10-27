a189414967fa598a369bd1e098a2e73cf41b81dd
Thara-PSU <73512825+Thara-PSU@users.noreply.github.com>
2020-10-27 14:12:38 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def main():
+
+
+    st.title("Machine learning application for predicting 2-year mortality of GBM ")
+    st.sidebar.title("Machine learning application for predicting 2-year mortality of GBM")
+    st.markdown("Results of machine learing")
+    st.sidebar.markdown("Algorithm of machine learing")
+
+
+if __name__ == '__main__':
+        main()
